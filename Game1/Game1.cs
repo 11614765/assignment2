d5b45c7cbcc2a24829535ea3819b9f2a576f9e4a
JiajianFeng <fengcent@gmail.com>
2015-10-28 12:59:18 +1100

    宋江

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -7,7 +7,7 @@ using System;
 namespace Game1
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// This is the main type for your game.宋江
     /// </summary>
     public class Game1 : Game
     {
